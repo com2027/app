cd39b6a4fc39c1d3f4682564828b5caeb876566f
Azwad Rashid <azu.rashid1998@gmail.com>
2019-04-16 14:23:13 +0100

    Added test methods to check if correct texts are being displayed and if the text fields work correctly

---
+++ app/src/androidTest/java/com/kierigby/bountyhunter/NewPasswordActivityTest.java
@@ -0,0 +1,34 @@
+package com.kierigby.bountyhunter;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withHint;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@RunWith(AndroidJUnit4.class)
+public class NewPasswordActivityTest {
+    @Rule public final ActivityRule<NewPasswordActivity> newPassRule = new ActivityRule<> (NewPasswordActivity.class);
+
+    @Test
+    public void checkCorrectTexts() {
+        onView(withText("New Password")).check(matches(isDisplayed()));
+        onView(withHint("New Password")).check(matches(isDisplayed()));
+        onView(withHint("Confirm New Password")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkTextFields() {
+        onView(withId(R.id.etNewPassword)).perform(typeText("Password1"));
+        onView(withId(R.id.etConfirmNewPassword)).perform(typeText("Password1"));
+    }
+
+}
