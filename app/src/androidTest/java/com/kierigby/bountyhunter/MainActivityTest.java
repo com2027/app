744a3fbf79ae5902c27f2843259849533304819b
Azwad Rashid <azu.rashid1998@gmail.com>
2019-04-20 17:49:46 +0100

    Updated espresso to its latest version

--- app/src/androidTest/java/com/kierigby/bountyhunter/MainActivityTest.java
+++ app/src/androidTest/java/com/kierigby/bountyhunter/MainActivityTest.java
@@ -7,18 +7,23 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withHint;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static com.kierigby.bountyhunter.CustomMatchers.withDrawableId;
 
-@RunWith(AndroidJUnit4.class)
+
 public class MainActivityTest {
-    @Rule public final ActivityRule<MainActivity> mainActivityRule = new ActivityRule<>(MainActivity.class);
+    @Rule public final ActivityTestRule<MainActivity> mainActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void checkCorrectTexts() {
@@ -38,4 +43,5 @@ public class MainActivityTest {
     public void checkCorrectDrawables() {
         onView(withId(R.id.logoImageView)).check(matches(withDrawableId(R.drawable.logo_with_marker)));
     }
+
 }
