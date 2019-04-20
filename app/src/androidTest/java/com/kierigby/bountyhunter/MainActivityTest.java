510ba83081aa94ce37fbb057594b1799baab7264
Azwad Rashid <azu.rashid1998@gmail.com>
2019-04-20 17:51:09 +0100

    Updated espresso to its latest version on MainActivityTest

--- app/src/androidTest/java/com/kierigby/bountyhunter/MainActivityTest.java
+++ app/src/androidTest/java/com/kierigby/bountyhunter/MainActivityTest.java
@@ -1,20 +1,13 @@
 package com.kierigby.bountyhunter;
 
-
-import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
-import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
