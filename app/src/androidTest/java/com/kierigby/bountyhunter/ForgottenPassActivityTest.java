94b1da4b4ef566c26176b1746b3fda070df4c9da
Azwad Rashid <azu.rashid1998@gmail.com>
2019-04-20 17:53:11 +0100

    Updated espresso to its latest version on ForgottenPassActivity Test

---
+++ app/src/androidTest/java/com/kierigby/bountyhunter/ForgottenPassActivityTest.java
@@ -0,0 +1,34 @@
+package com.kierigby.bountyhunter;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+
+public class ForgottenPassActivityTest {
+    @Rule public final ActivityTestRule<ForgottenPassActivity> forgottenPassRule = new ActivityTestRule<>(ForgottenPassActivity.class);
+
+    @Test
+    public void checkCorrectText() {
+        onView(withText("Forgotten Password")).check(matches(isDisplayed()));
+        onView(withText("We can help you recover your password, but first we need a few details from you...")).check(matches(isDisplayed()));
+        onView(withHint("Please enter your email")).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void checkTextFields() {
+        onView(withId(R.id.enterEmail)).perform(typeText("johnsmith123@gmail.com"));
+
+    }
+
+}
