21bb7f994f4ba09132524e17ce8115a2d7a02ebd
Azwad Rashid <azu.rashid1998@gmail.com>
2019-04-20 17:48:59 +0100

    Added intent tests for buttons on MainActivity

--- app/src/androidTest/java/com/kierigby/bountyhunter/MainActivityIntentTest.java
+++ app/src/androidTest/java/com/kierigby/bountyhunter/MainActivityIntentTest.java
@@ -1,33 +1,22 @@
 package com.kierigby.bountyhunter;
 
-
-
 import androidx.test.espresso.intent.rule.IntentsTestRule;
 
-
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-
-
 import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.clearText;
 import static androidx.test.espresso.action.ViewActions.click;
 
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.action.ViewActions.typeText;
-import static androidx.test.espresso.action.ViewActions.typeTextIntoFocusedView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
-
-
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
 import static androidx.test.espresso.intent.Intents.intended;
 import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 
 public class MainActivityIntentTest {
+
     @Rule
     public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
 
@@ -52,4 +41,5 @@ public class MainActivityIntentTest {
         Thread.sleep(500);
         intended(hasComponent(LoggedInActivity.class.getName()));
     }
+
 }
